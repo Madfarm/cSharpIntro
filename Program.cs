8119677c9eb12ac1915e1da0f1b553a72bcc4429
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-04 16:06:49 -0400

    Add the boilerplate

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FirstGo
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("did it work?");
+        }
+    }
+}
--- notes.md
+++ notes.md
@@ -4,3 +4,10 @@ dotnet new and then the template of choice to get started
 
 > dotnet new console 
 
+
+add a built-in gitignore
+> dotnet new gitignore
+
+
+> dotnet run
+
